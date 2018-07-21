d8299ef7d664507199b600fb4928163b4036fea0
Keir Bowden <keirbowden@brightgen.com>
2018-07-21 16:25:34 +0100

    Added functionality to extract folder metadata.

--- commands/export.js
+++ commands/export.js
@@ -46,7 +46,9 @@ const allMetadata=[
     "CustomSite",
     "CustomTab",
     "DataCategoryGroup",
+    "Dashboard",
     "DuplicateRule",
+    "EmailTemplate",
     "EntitlementProcess",
     "EntitlementSettings",
     "EntitlementTemplate",
@@ -86,12 +88,15 @@ const allMetadata=[
     "RecordType",
     "RemoteSiteSetting",
     "ReportType",
+    "Report",
     "Role",
     "SamlSsoConfig",
     "Scontrol",
     "SecuritySettings",
     "SharingReason",
-    "SharingRules",
+    "SharingCriteriaRule",
+    "SharingOwnerRule",
+    "SharingTerritoryRule",
     "Skill",
     "StaticResource",
     "Territory",
@@ -121,23 +126,65 @@ function ExportCommand(opts) {
 
 ExportCommand.prototype.execute = function () {
     console.log('Exporting metadata');
+    this.getFolderMetadata();
     var standardObjects=this.getStandardObjects();
-
     this.startPackage(); 
     for (var idx=0; idx<allMetadata.length; idx++) {
         var mdName=allMetadata[idx];
         this.startTypeInPackage();
-        if (mdName=='CustomObject') {
-            for (var soIdx=0; soIdx<standardObjects.length; soIdx++) {
-                this.addPackageMember(standardObjects[soIdx]);
-            }
+        switch(mdName) {
+            case 'Dashboard':
+                var dbFolders=this.foldersByType['Dashboard'];
+                for (var folderId in dbFolders) {
+                    if (dbFolders.hasOwnProperty(folderId)) {
+                        var folder=dbFolders[folderId];
+                        this.addPackageMember(folder.Name);
+                        for (var dbIdx=0; dbIdx<folder.members.length; dbIdx++) {
+                            var dashboard=folder.members[dbIdx];
+                            this.addPackageMember(folder.Name + '/' + dashboard.DeveloperName);
+                        }
+                    }
+                }
+                break;
+            case 'EmailTemplate':
+                var etFolders=this.foldersByType['Email'];
+                for (var folderId in etFolders) {
+                    if (etFolders.hasOwnProperty(folderId)) {
+                        var folder=etFolders[folderId];
+                        this.addPackageMember(folder.Name);
+                        for (var etIdx=0; etIdx<folder.members.length; etIdx++) {
+                            var emailTemplate=folder.members[etIdx];
+                            this.addPackageMember(folder.Name + '/' + emailTemplate.DeveloperName);
+                        }
+                    }
+                }
+                break;
+            case 'Report':
+                var repFolders=this.foldersByType['Report'];
+                for (var folderId in repFolders) {
+                    if (repFolders.hasOwnProperty(folderId)) {
+                        var folder=repFolders[folderId];
+                        this.addPackageMember(folder.Name);
+                        for (var repIdx=0; repIdx<folder.members.length; repIdx++) {
+                            var report=folder.members[repIdx];
+                            this.addPackageMember(folder.Name + '/' + report.DeveloperName);
+                        }
+                    }
+                }
+                break;
+            case 'CustomObject': 
+                for (var soIdx=0; soIdx<standardObjects.length; soIdx++) {
+                    this.addPackageMember(standardObjects[soIdx]);
+                }
+            default:
+                this.addPackageMember('*');
         }
-        this.addPackageMember('*');
         this.endTypeInPackage(mdName);
     }  
 
     this.endPackage();
 
+    console.log('Extracting metadata');
     child_process.execFileSync('sfdx', 
                     ['force:mdapi:retrieve', 
                         '-u', this.options.sfdxUser, 
@@ -199,6 +246,97 @@ ExportCommand.prototype.startTypeInPackage = function(cfg)
 ExportCommand.prototype.endTypeInPackage = function(name) {
     fse.appendFileSync(this.packageFilePath, '    <name>' + name + '</name>\n' +
                                          '  </types>\n');
+}
+
+ExportCommand.prototype.getFolderMetadata = function() {
+    this.buildFolderStructure();
+    this.getReports(); 
+    this.getDashboards(); 
+    this.getEmailTemplates(); 
+}
+
+ExportCommand.prototype.buildFolderStructure = function() {
+    let query="Select Id, Name, DeveloperName, Type, NamespacePrefix from Folder where DeveloperName!=null";
+    let foldersJSON=child_process.execFileSync('sfdx', 
+        ['force:data:soql:query',
+            '-q', query, 
+            '-u', this.options.sfdxUser,
+            '--json']);
+
+    let folders=JSON.parse(foldersJSON);
+    this.foldersByType={'Dashboard':{},
+                       'Report':{}, 
+                       'Email':{}}
+
+    for (var idx=0; idx<folders.result.records.length; idx++) {
+        var folder=folders.result.records[idx];
+        var foldersForType=this.foldersByType[folder.Type];
+        if (foldersForType) {
+            if (!foldersForType[folder.Id]) {
+                foldersForType[folder.Id]={'Name': folder.DeveloperName, 'members': []};
+            }
+        }
+    }
+}
+
+ExportCommand.prototype.getReports = function() {
+    let query="Select Id, DeveloperName, OwnerId from Report";
+    let reportsJSON=child_process.execFileSync('sfdx', 
+        ['force:data:soql:query',
+            '-q', query, 
+            '-u', this.options.sfdxUser,
+            '--json']);
+
+    let reports=JSON.parse(reportsJSON);
+
+    for (var idx=0; idx<reports.result.records.length; idx++) {
+        var report=reports.result.records[idx];
+        var foldersForReports=this.foldersByType['Report'];
+        var folderForThisReport=foldersForReports[report.OwnerId];
+        if (folderForThisReport) {
+            folderForThisReport.members.push(report);
+        }
+    }
+}
+
+ExportCommand.prototype.getDashboards = function() {
+    let query="Select Id, DeveloperName, FolderId from Dashboard";
+    let dashboardsJSON=child_process.execFileSync('sfdx', 
+        ['force:data:soql:query',
+            '-q', query, 
+            '-u', this.options.sfdxUser,
+            '--json']);
+
+    let dashboards=JSON.parse(dashboardsJSON);
+
+    for (var idx=0; idx<dashboards.result.records.length; idx++) {
+        var dashboard=dashboards.result.records[idx];
+        var foldersForDashboards=this.foldersByType['Dashboard'];
+        var folderForThisDashboard=foldersForDashboards[dashboard.FolderId];
+        if (folderForThisDashboard) {
+            folderForThisDashboard.members.push(dashboard);
+        }
+    }
+}
+
+ExportCommand.prototype.getEmailTemplates = function() {
+    let query="Select Id, DeveloperName, FolderId from EmailTemplate";
+    let templateJSON=child_process.execFileSync('sfdx', 
+        ['force:data:soql:query',
+            '-q', query, 
+            '-u', this.options.sfdxUser,
+            '--json']);
+
+    let templates=JSON.parse(templateJSON);
+
+    for (var idx=0; idx<templates.result.records.length; idx++) {
+        var template=templates.result.records[idx];
+        var foldersForTemplates=this.foldersByType['Email'];
+        var folderForThisTemplate=foldersForTemplates[template.FolderId];
+        if (folderForThisTemplate) {
+            folderForThisTemplate.members.push(template);
+        }
+    }
 }
 
 module.exports=ExportCommand;
